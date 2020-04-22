c09aadd1607c6e112a402786f4fa6bf97eb1c8a5
Moncs <eqopendkp@gmail.com>
2020-04-22 17:18:40 -0400

    initial update

---
+++ main.ts
@@ -0,0 +1,180 @@
+import { app, BrowserWindow, screen, ipcMain, dialog } from 'electron';
+import * as path from 'path';
+import * as url from 'url';
+const Tail = require('tail').Tail;
+const Store = require('electron-store');
+
+
+let win: BrowserWindow = null;
+const args = process.argv.slice(1),
+    serve = args.some(val => val === '--serve');
+const WebSocket = require('ws');
+let wss;
+let clients = [];
+let fPath = '';
+let fChannel = '';
+let options = { separator: /[\r]{0,1}\n/, fromBeginning: false, fsWatchOptions: {}, follow: true, logger: console }
+let fTail;
+
+function createWindow(): BrowserWindow {
+    const store = new Store();
+    const electronScreen = screen;
+    const size = electronScreen.getPrimaryDisplay().workAreaSize;
+
+    // Create the browser window.
+    win = new BrowserWindow({
+        x: 0,
+        y: 0,
+        width: size.width,
+        height: size.height,
+        webPreferences: {
+            nodeIntegration: true,
+            allowRunningInsecureContent: (serve) ? true : false,
+        },
+    });
+
+    if (serve) {
+        require('electron-reload')(__dirname, {
+            electron: require(`${__dirname}/node_modules/electron`)
+        });
+        win.loadURL('http://localhost:4300');
+    } else {
+        win.loadURL(url.format({
+            pathname: path.join(__dirname, 'dist/index.html'),
+            protocol: 'file:',
+            slashes: true
+        }));
+    }
+
+    if (serve) {
+        win.webContents.openDevTools();
+    }
+
+    // Emitted when the window is closed.
+    win.on('closed', () => {
+        // Dereference the window object, usually you would store window
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null;
+    });
+
+    //Set some Defaults for the Window from User Preferences
+    fPath = store.get('path');
+    fChannel = store.get('channel');
+    //Should switch to event: ready-to-show???
+    win.webContents.on('did-finish-load', () => {
+        if (fPath) win.webContents.send('set_path', fPath);
+        if (fChannel) win.webContents.send('set_channel', fChannel);
+        initWebSocketServer();
+    });
+
+    //List for the webContent to open a dialog
+    ipcMain.addListener('open_dialog', (event) => {
+        dialog.showOpenDialog({
+            properties: ['openFile']
+        }).then(result => {
+            fPath = result.filePaths[0];
+            store.set('path', fPath);
+            win.webContents.send('path', fPath);
+        }).catch(err => {
+            console.log(err)
+        });
+    });
+
+    ipcMain.addListener('start_monitoring', (event, channel) => {
+        store.set('channel', channel);
+        fChannel = channel;
+        startMonitoring()
+    });
+
+    return win;
+}
+
+function initWebSocketServer() {
+    if (!wss) {
+        wss = new WebSocket.Server({ port: 8080 });
+        wss.on('connection', function connection(ws) {
+            win.webContents.send('log_output', 'connection opened from browser');
+            ws.on('message', function incoming(message) {
+                console.log('received: %s', message);
+            });
+            clients.push(ws);
+        });
+
+        wss.on('listening', () => {
+            win.webContents.send('log_output', 'Websocket Server is bound');
+        });
+    }
+}
+
+function startMonitoring() {
+    try {
+        fTail = new Tail(fPath, options);
+        win.webContents.send('log_output', `Tailing log file: ${fPath}`);
+
+        fTail.on("line", function(data) {
+            if (data.indexOf(fChannel) > 0) {
+                data = data.substring(data.indexOf(', \'') + 3, data.length - 1);
+                data = data.trim();
+                var items = data.split('|');
+                var vSearchItems = [];
+                if (items.length > 0) {
+                    items.forEach(x => {
+                        x = x.trim();
+                        if (x && x.length > 0) {
+                            vSearchItems.push(x);
+                        }
+                    });
+                    if (vSearchItems.length > 0) {
+                        win.webContents.send('new_items', vSearchItems);
+                        sendClientMessage(vSearchItems);
+                    }
+                }
+            }
+        });
+        fTail.on("error", function(error) {
+            console.log('ERROR: ', error);
+            win.webContents.send('log_output', `Error ${error}`);
+        });
+    } catch (error) {
+        win.webContents.send('log_output', `Erorr trying to tail log file: ${fPath}`);
+        win.webContents.send('log_output', error);
+    }
+}
+
+function sendClientMessage(data) {
+    clients.forEach(element => {
+        element.send(JSON.stringify(data));
+    });
+}
+
+try {
+
+    app.allowRendererProcessReuse = true;
+
+    // This method will be called when Electron has finished
+    // initialization and is ready to create browser windows.
+    // Some APIs can only be used after this event occurs.
+    // Added 400 ms to fix the black background issue while using transparent window. More detais at https://github.com/electron/electron/issues/15947
+    app.on('ready', () => setTimeout(createWindow, 400));
+
+    // Quit when all windows are closed.
+    app.on('window-all-closed', () => {
+        // On OS X it is common for applications and their menu bar
+        // to stay active until the user quits explicitly with Cmd + Q
+        if (process.platform !== 'darwin') {
+            app.quit();
+        }
+    });
+
+    app.on('activate', () => {
+        // On OS X it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (win === null) {
+            createWindow();
+        }
+    });
+} catch (e) {
+    // Catch Error
+    // throw e;
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+import { ElectronService } from '../core/services';
+
+@Component({
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+    public logOutput: string = "";
+    public path: string = "";
+    public channel: string = "";
+
+    constructor(private router: Router,
+        private electronService: ElectronService,
+        private zone: NgZone) {}
+
+    ngOnInit(): void {
+        this.electronService.ipcRenderer.addListener('new_items', (event, item) => {
+            this.zone.run(() => {
+                item.forEach(x => {
+                    this.logOutput += `Item Detected: ${x}\n`;
+                });
+            })
+        });
+        this.electronService.ipcRenderer.addListener('path', (event, path) => {
+            this.zone.run(() => {
+                this.logOutput += `Log Path set to: ${path}\n`;
+                this.path = path;
+            })
+        });
+        this.electronService.ipcRenderer.addListener('set_path', (event, path) => {
+            this.zone.run(() => {
+                this.logOutput += `Stored Path set to: ${path}\n`;
+                this.path = path;
+            })
+        });
+        this.electronService.ipcRenderer.addListener('set_channel', (event, channel) => {
+            this.zone.run(() => {
+                this.logOutput += `Stored Channel set to: ${channel}\n`;
+                this.channel = channel;
+            })
+        });
+        this.electronService.ipcRenderer.addListener('log_output', (event, message) => {
+            this.zone.run(() => {
+                this.logOutput += `${message}\n`;
+            })
+        });
+    }
+
+    openFile() {
+        this.electronService.ipcRenderer.send('open_dialog');
+    }
+
+    startMonitor() {
+      this.channel = this.channel.trim();
+      if (this.channel.length <= 0) {
+          alert('you must first set a chat channel to be monitored');
+          return;
+      }
+      this.electronService.ipcRenderer.send('start_monitoring', this.channel);
+    }
+}
